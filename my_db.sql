c3c0982f317d64e8a14efbebefc04dfd637fecf8
Rikysamuel <rikz.samuel@gmail.com>
2016-02-19 12:29:21 +0700

    edit db structure

--- my_db.sql
+++ my_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
+-- version 4.4.12
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2014 at 07:54 AM
--- Server version: 5.6.11
--- PHP Version: 5.5.1
+-- Generation Time: Feb 19, 2016 at 06:20 AM
+-- Server version: 5.6.25
+-- PHP Version: 5.6.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,13 +14,11 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `my_db`
 --
-CREATE DATABASE IF NOT EXISTS `my_db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `my_db`;
 
 -- --------------------------------------------------------
 
@@ -32,8 +30,7 @@ CREATE TABLE IF NOT EXISTS `komentar` (
   `id` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `komentar` varchar(255) NOT NULL,
-  `tanggal` date NOT NULL,
-  KEY `id` (`id`)
+  `tanggal` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -43,13 +40,61 @@ CREATE TABLE IF NOT EXISTS `komentar` (
 --
 
 CREATE TABLE IF NOT EXISTS `posting` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int(11) NOT NULL,
   `JUDUL` varchar(100) DEFAULT NULL,
   `TANGGAL` date DEFAULT NULL,
   `KONTEN` varchar(8000) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=44 ;
+  `username` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `Name` varchar(255) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `email` int(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` int(1) NOT NULL,
+  `n` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `komentar`
+--
+ALTER TABLE `komentar`
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `posting`
+--
+ALTER TABLE `posting`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Username`),
+  ADD KEY `Username` (`Username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posting`
+--
+ALTER TABLE `posting`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=44;
 --
 -- Constraints for dumped tables
 --
@@ -59,6 +104,12 @@ CREATE TABLE IF NOT EXISTS `posting` (
 --
 ALTER TABLE `komentar`
   ADD CONSTRAINT `komentar_ibfk_1` FOREIGN KEY (`id`) REFERENCES `posting` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `posting`
+--
+ALTER TABLE `posting`
+  ADD CONSTRAINT `posting_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`Username`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
